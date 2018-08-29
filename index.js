41daae5006f38cfcd1f8c10f603131e6f0b92be9
WilliamKasurak <willkdesign@gmail.com>
2018-08-29 21:30:21 +0000

    eureka

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  if(!(fromN > toN)){
+    if (fromN == toN) {
+      return fromN;
+    } else {
+      let  num = fromN;
+    return( fromN +  sum( fromN + 1 , toN) )
+    }
+  }
 }
 
+console.log(  sum(3,7)  );
+
+
+
+// sum(6,7) = 6 + sum(6+1 , 7)
+         // = 6 + 7
+
 module.exports = sum;
+
+
+// 3 + 4 + 5 + 6 + 7
+//
